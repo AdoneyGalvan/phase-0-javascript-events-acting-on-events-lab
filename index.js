60fae69eacb1e003aa2b4a8a73d9fdc92c06e1bf
Aron Galvan <arongalvan03@gmail.com>
2022-06-04 13:52:10 -0700

    commit tree 3368ee44c16e570e121c7b6461c90764905e6d73

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+
+
+dodger.addEventListener('keydown', (e) => {
+    console.log(e.key);
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight")
+    {
+        moveDodgerRight();
+    }
+});
+
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+const  moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 400) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
